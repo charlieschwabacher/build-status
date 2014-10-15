f75a2b985ae7bf044c4e04e28d60e26b388029b9
charlieschwabacher <charlieschwabacher@gmail.com>
2014-10-14 17:18:24 -0700

    work

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ module.exports = {
   server: function() {
     var WebSocketServer = require('ws').Server;
     var wss = new WebSocketServer({port: 8087});
+    var lastMessage = '';
+
+    wss.on('connection', function(ws) {
+      ws.send(lastMessage);
+    });
 
     return {
       send: function(message) {
+        lastMessage = message;
         wss.clients.forEach(function(client){
           try {
             client.send(message);
@@ -21,7 +27,7 @@ module.exports = {
     ws.onmessage = function(event) {
       if (event.data == 'reload') {
         window.location.reload();
-      } else {
+      } else if (event.data) {
         var classes = Array.prototype.slice.call(document.body.classList, 0);
         classes.forEach(function(className) {
           if (className.match(/$build-status-/)) {
